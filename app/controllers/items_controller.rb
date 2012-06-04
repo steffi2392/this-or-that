9c8cea04c26f67c394ac9c271ba9a66b5da7c315
Steffi <steffi.ostrowski@gmail.com>
2012-06-04 09:43:50 -0400

    image_url

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -125,6 +125,14 @@ class ItemsController < ApplicationController
 	  	
 	  	return request.env['omniauth.auth']
 	  end
+	  
+	  def image_url(source)
+	    abs_path = image_path(source)
+	    unless abs_path =~ /^http/
+	      abs_path = "#{request.protocol}#{request.host_with_port}#{abs_path}"
+	    end
+	   abs_path
+	  end
 
 
 end
